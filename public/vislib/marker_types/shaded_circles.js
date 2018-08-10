60acf490cda8a659c33bbfe477749bcea16415f0
Oussama Sakri <oussama.sakri@supcom.tn>
2018-08-10 11:52:58 +0200

    Update shaded_circles.js

--- public/vislib/marker_types/shaded_circles.js
+++ public/vislib/marker_types/shaded_circles.js
@@ -18,8 +18,8 @@ define(function (require) {
       ShadedCircleMarker.Super.apply(this, arguments);
 
       // super min and max from all chart data
-      let min = this.geoJson.properties.allmin;
-      let max = this.geoJson.properties.allmax;
+      let min = this.geoJson.properties.min;
+      let max = this.geoJson.properties.max;
 
       // multiplier to reduce size of all circles
       let scaleFactor = 0.8;
